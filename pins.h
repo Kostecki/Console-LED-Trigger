15a52294ed3dc752b8948d26ebe207ca0e50d244
Jacob Kostecki <jacob@kostecki.dk>
2025-07-27 19:36:22 +0200

    change switch/encoder pins

--- pins.h
+++ pins.h
@@ -2,8 +2,8 @@
 
 constexpr uint8_t LED_PIN = 29;
 
-constexpr uint8_t ENCODER_A = 6;
-constexpr uint8_t ENCODER_B = 7;
-constexpr uint8_t ENCODER_SW = 8;
+constexpr uint8_t ENCODER_A = 5;
+constexpr uint8_t ENCODER_B = 6;
+constexpr uint8_t ENCODER_SW = 7;
 
 constexpr uint8_t CURRENT_SENSE_PIN = 27;
