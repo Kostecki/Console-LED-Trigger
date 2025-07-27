4c1af83b01ccc0574c71a446f40830701493433a
Jacob Kostecki <jacob@kostecki.dk>
2025-07-27 19:35:06 +0200

    changes offsets and delays

--- config.h
+++ config.h
@@ -8,8 +8,8 @@
 constexpr uint8_t NUM_PIXELS = 15;
 
 // Current Sense Config
-const int CURRENT_THRESHOLD = 100;
-const int CURRENT_SENSE_OFFSET = 10;
+const int CURRENT_THRESHOLD = 150;
+const int CURRENT_SENSE_OFFSET = 15;
 const int CURRENT_THRESHOLD_ON = CURRENT_THRESHOLD + CURRENT_SENSE_OFFSET;
 const int CURRENT_THRESHOLD_OFF = CURRENT_THRESHOLD - CURRENT_SENSE_OFFSET;
 
@@ -17,5 +17,5 @@ const int CURRENT_THRESHOLD_OFF = CURRENT_THRESHOLD - CURRENT_SENSE_OFFSET;
 constexpr int ENCODER_STEPS_PER_CLICK = 4;
 
 // Timing Config
-constexpr uint8_t LONG_PRESS_THRESHOLD = 2000; // 2 seconds
-constexpr uint8_t POWER_OFF_DELAY = 5000;      // 5 seconds
+constexpr unsigned long LONG_PRESS_THRESHOLD = 2000; // 2 seconds
+constexpr unsigned long POWER_OFF_DELAY = 1000;      // 1 second
