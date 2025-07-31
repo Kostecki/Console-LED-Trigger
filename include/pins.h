9b336151d5536445688bb6fdbf7ef8403fbd7ac0
Jacob Kostecki <jacob@kostecki.dk>
2025-07-31 16:51:39 +0200

    adjust pins

--- include/pins.h
+++ include/pins.h
@@ -1,9 +1,9 @@
 #pragma once
 
-constexpr uint8_t LED_PIN = 5;
+constexpr uint8_t CURRENT_SENSE_PIN = 1;
 
-constexpr uint8_t ENCODER_A = 7;
-constexpr uint8_t ENCODER_B = 8;
-constexpr uint8_t ENCODER_SW = 9;
+constexpr uint8_t LED_PIN = 4;
 
-constexpr uint8_t CURRENT_SENSE_PIN = 3;
+constexpr uint8_t ENCODER_A = 5;
+constexpr uint8_t ENCODER_B = 6;
+constexpr uint8_t ENCODER_SW = 7;
