28a4a6a7c635c36a573ad3731d84dfd47fde6919
Jacob Kostecki <jacob@kostecki.dk>
2025-08-08 19:39:04 +0200

    log mqtt

--- dashboard/context/MqttProvider.tsx
+++ dashboard/context/MqttProvider.tsx
@@ -14,15 +14,19 @@ export const MqttProvider = ({ children }: { children: ReactNode }) => {
 	const [client, setClient] = useState<MqttClient | null>(null);
 	const clientRef = useRef<MqttClient | null>(null);
 
+	const MQTT_URL = import.meta.env.VITE_MQTT_URL;
+	const MQTT_USERNAME = import.meta.env.VITE_MQTT_USERNAME;
+	const MQTT_PASSWORD = import.meta.env.VITE_MQTT_PASSWORD; // This isn't actually very secure, but it's fine for lan only 🤷‍♂️
+
 	useEffect(() => {
 		if (clientRef.current) {
 			return;
 		}
 
-		const mqttClient = mqtt.connect(import.meta.env.VITE_MQTT_URL, {
+		const mqttClient = mqtt.connect(MQTT_URL, {
 			clientId: `dashboard-${Math.random().toString(16).slice(2)}`,
-			username: import.meta.env.VITE_MQTT_USERNAME,
-			password: import.meta.env.VITE_MQTT_PASSWORD, // This isn't actually very secure, but it's fine for lan only 🤷‍♂️
+			username: MQTT_USERNAME,
+			password: MQTT_PASSWORD,
 			reconnectPeriod: 1000,
 		});
 
@@ -30,6 +34,7 @@ export const MqttProvider = ({ children }: { children: ReactNode }) => {
 
 		const handleConnect = () => {
 			console.log("MQTT client connected");
+			console.log("URL: ", MQTT_URL);
 			setClient(mqttClient);
 		};
 
